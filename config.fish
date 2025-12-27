dbb49200eb5257fdeaa201617d471c45df9a60ca
Oskar Idland <oskaridland@hotmail.com>
2025-12-27 02:01:20 +0100

    move to old computer

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    # --- Abbreviations and Aliases ---
+    
     ### KOBE ###
     abbr --add ssh-a 'ssh -i ~/.ssh/lan_kyoto_hpc b38535@camphor.kudpc.kyoto-u.ac.jp'
     abbr --add ssh-b 'ssh -i ~/.ssh/lan_kyoto_hpc b38535@laurel.kudpc.kyoto-u.ac.jp'
@@ -9,6 +11,10 @@ if status is-interactive
 
     ### STARSHIP ###
     starship init fish | source
+
+    ### DISPLAY MANAGER
+    set -l DA hyprlock
+    abbr --add reload_da "hyprctl --instance 0 'keyword misc:allow_session_lock_restore 1' ; hyprctl --instance 0 'dispatch exec $DA'"
     
     ### FISH ###
     #set fish_cursor_default block
@@ -50,6 +56,7 @@ if status is-interactive
     ### EDITOR  ###
     abbr --add mc micro
     abbr --add mcr 'micro -readonly on'
+    abbr --add mcf 'micro (fzf)'
 
     ### PYTHON  ###
 #   abbr --add cnda 'conda init fish &> /dev/null ; source ~/.config/fish/config.fish ; conda activate myenv'
@@ -85,7 +92,12 @@ if status is-interactive
     ### SOFTWARE ###
     alias menk='microsoft-edge-stable --force-device-scale-factor=2.0 ~/repos/Knowlegde-Vault/D\&D/Menk_character_sheet.pdf > /dev/null 2>&1 & disown ; exit'
     
-    ### PLUGINS  ###
+    # --- Envirnoment ---
+    # ROOT
+    source ~/root_src_v.6.36.02/BUILD/bin/thisroot.fish
+
+     ~/geant4-v11.3.2-installQt/bin/geant4.sh
+    
     # LS_COLORS
     setenv LS_COLORS 'rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=00:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.avif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:*~=00;90:*#=00;90:*.bak=00;90:*.crdownload=00;90:*.dpkg-dist=00;90:*.dpkg-new=00;90:*.dpkg-old=00;90:*.dpkg-tmp=00;90:*.old=00;90:*.orig=00;90:*.part=00;90:*.rej=00;90:*.rpmnew=00;90:*.rpmorig=00;90:*.rpmsave=00;90:*.swp=00;90:*.tmp=00;90:*.ucf-dist=00;90:*.ucf-new=00;90:*.ucf-old=00;90:'
     source /usr/share/LS_COLORS/dircolors.csh
@@ -96,11 +108,13 @@ if status is-interactive
     ### MAN  ###
     set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
     set -x MANROFFOPT "-c"
+
+    ### NMTUI ###
+    set -x NEWT_COLORS 'root=black;window=black,black;border=white,black;label=blue,black;checkbox=red,black;title=green,black;button=white,red;actsellistbox=white,red;actcheckbox=white,blue;entry=lightgray,black;textbox=blue,black' nmtui
+
     
     ### OTHER ###
     alias fresh='reload-fish ; clear ; fish_greeting'
-    
-
 
     set -Ux QT_DEVICE_PIXEL_RATIO 0
     set -Ux QT_AUTO_SCREEN_SCALE_FACTOR 1
